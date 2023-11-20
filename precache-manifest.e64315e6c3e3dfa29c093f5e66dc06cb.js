4d6d114a0ccc1c66189404e9874ef3d5df157574
Amit Bhardwaj <mramaj647@gmail.com>
2023-11-20 20:18:45 +0530

    Updates

---
+++ precache-manifest.e64315e6c3e3dfa29c093f5e66dc06cb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "679e35c90ee1ba870a2a",
+    "url": "/Google-Keeper/static/js/main.679e35c9.chunk.js"
+  },
+  {
+    "revision": "bf84d02e3d15a2cc4d9f",
+    "url": "/Google-Keeper/static/js/1.bf84d02e.chunk.js"
+  },
+  {
+    "revision": "891358736a582a1c135a",
+    "url": "/Google-Keeper/static/js/runtime~main.89135873.js"
+  },
+  {
+    "revision": "80cc02c6e550094e331185c17d8f873d",
+    "url": "/Google-Keeper/index.html"
+  }
+];
